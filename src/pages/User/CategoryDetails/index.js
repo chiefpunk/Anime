e99eb591ca48ee7ab12c009bcd322cabc62eaa89
WebDevStar <webdevstar14@gmail.com>
2020-10-20 04:35:43 +1000

    Update CategoryDetails

--- src/pages/User/CategoryDetails/index.js
+++ src/pages/User/CategoryDetails/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import VIDEO_DATA from "./videos";
 import BreadcrumbOption from "components/BreadcrumbOption";
 import Card from "components/Card";
