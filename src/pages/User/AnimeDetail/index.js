6cefae31cbc3ab9ccef607ff3cfe8228b3592a91
WebDevStar <webdevstar14@gmail.com>
2020-10-20 11:53:27 +1000

    Update Initialize

--- src/pages/User/AnimeDetail/index.js
+++ src/pages/User/AnimeDetail/index.js
@@ -9,29 +9,29 @@ export default function Index(props) {
   const { title } = useParams();
   return (
     <>
-      <section class="anime-details spad">
-        <div class="container">
-          <div class="anime__details__content">
-            <div class="row">
-              <div class="col-lg-3">
+      <section className="anime-details spad">
+        <div className="container">
+          <div className="anime__details__content">
+            <div className="row">
+              <div className="col-lg-3">
                 <div
-                  class="anime__details__pic set-bg"
+                  className="anime__details__pic set-bg"
                   data-setbg="img/anime/details-pic.jpg"
                   style={{
                     background: `url(${require("assets/img/anime/details-pic.jpg")})`,
                   }}
                 >
-                  <div class="comment">
-                    <i class="fa fa-comments"></i> 11
+                  <div className="comment">
+                    <i className="fa fa-comments"></i> 11
                   </div>
-                  <div class="view">
-                    <i class="fa fa-eye"></i> 9141
+                  <div className="view">
+                    <i className="fa fa-eye"></i> 9141
                   </div>
                 </div>
               </div>
-              <div class="col-lg-9">
-                <div class="anime__details__text">
-                  <div class="anime__details__title">
+              <div className="col-lg-9">
+                <div className="anime__details__text">
+                  <div className="anime__details__title">
                     <h3>{title}</h3>
                     <span>フェイト／ステイナイト, Feito／sutei naito</span>
                   </div>
@@ -43,9 +43,9 @@ export default function Index(props) {
                     send Hina on a quest to find a legendary hero from the Waste
                     War - the fabled Ace!
                   </p>
-                  <div class="anime__details__widget">
-                    <div class="row">
-                      <div class="col-lg-6 col-md-6">
+                  <div className="anime__details__widget">
+                    <div className="row">
+                      <div className="col-lg-6 col-md-6">
                         <ul>
                           <li>
                             <span>Type:</span> TV Series
@@ -65,7 +65,7 @@ export default function Index(props) {
                           </li>
                         </ul>
                       </div>
-                      <div class="col-lg-6 col-md-6">
+                      <div className="col-lg-6 col-md-6">
                         <ul>
                           <li>
                             <span>Scores:</span> 7.31 / 1,515
@@ -86,31 +86,31 @@ export default function Index(props) {
                       </div>
                     </div>
                   </div>
-                  <div class="anime__details__btn">
-                    <a class="follow-btn">
-                      <i class="fa fa-heart-o"></i> Follow
+                  <div className="anime__details__btn">
+                    <a href="/" className="follow-btn">
+                      <i className="fa fa-heart-o"></i> Follow
                     </a>
-                    <Link to={`/watch/${title}`} class="watch-btn">
-                      <span>Watch Now</span> <i class="fa fa-angle-right"></i>
+                    <Link to={`/watch/${title}`} className="watch-btn">
+                      <span>Watch Now</span> <i className="fa fa-angle-right"></i>
                     </Link>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-8 col-md-8">
-              <div class="anime__details__review">
+          <div className="row">
+            <div className="col-lg-8 col-md-8">
+              <div className="anime__details__review">
                 <SectionTitle tag="h5" title="REVIEWS" />
                 <ReviewItems />
               </div>
-              <div class="anime__details__form">
+              <div className="anime__details__form">
                 <SectionTitle tag="h5" title="Your Comment" />
                 <CommentForm />
               </div>
             </div>
-            <div class="col-lg-4 col-md-4">
-              <div class="anime__details__sidebar">
+            <div className="col-lg-4 col-md-4">
+              <div className="anime__details__sidebar">
                 <SectionTitle tag="h5" title="you might like..." />
                 <TopVideos />
               </div>
